ecb55d862997abcfcb2950ae4e46d95cd27d7b99
Papito <fndpapito@gmail.com>
2024-09-09 22:44:07 -0300

    vai papito

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "rounded group inline-flex h-10 w-max items-center justify-center bg-zinc-800 px-8 py-2 text-sm font-medium transition-colors hover:bg-zinc-700 focus:bg-zinc-70 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-zinc-700 data-[state=open]:bg-zinc-700"
 )
 
 const NavigationMenuTrigger = React.forwardRef<
